2ce60e772988729b2ee1d122db466f90336dc132
jsb5151 <jsblack@me.com>
2021-09-26 00:20:14 -0400

    update F7msg.h with macros for new bits

--- arduinoProj/USB2keybus/F7msg.h
+++ arduinoProj/USB2keybus/F7msg.h
@@ -4,18 +4,43 @@
 
 // F7 bit macros to simplify code
 #define SET_BIT(b,v,m)       (((b) & ~(m)) | ((v) ? (m) : (0x00)))
+
+#define SET_FIRE(b,v)        SET_BIT(b,v,0x01)  // BYTE2
+#define SET_CHECK_FLAG(b,v)  SET_BIT(b,v,0x02)  // BYTE2
+#define SET_SYSTEM_FLAG(b,v) SET_BIT(b,v,0x04)  // BYTE2  
+#define SET_AC_LOSS(b,v)     SET_BIT(b,v,0x08)  // BYTE2
 #define SET_READY(b,v)       SET_BIT(b,v,0x10)  // BYTE2
+#define SET_ALARM_ZONE(b,v)  SET_BIT(b,v,0x20)  // BYTE2
+#define SET_LOWBAT(b,v)      SET_BIT(b,v,0x40)  // BYTE2  
 #define SET_ARMED_STAY(b,v)  SET_BIT(b,v,0x80)  // BYTE2
+
+#define SET_IN_ALARM(b,v)    SET_BIT(b,v,0x01)  // BYTE3
+#define SET_SYS_ALARM(b,v)   SET_BIT(b,v,0x02)  // BYTE3
 #define SET_ARMED_AWAY(b,v)  SET_BIT(b,v,0x04)  // BYTE3
-#define SET_CHIME(b,v)       SET_BIT(b,v,0x20)  // BYTE3
 #define SET_POWER(b,v)       SET_BIT(b,v,0x08)  // BYTE3
+#define SET_BYPASS(b,v)      SET_BIT(b,v,0x10)  // BYTE3
+#define SET_CHIME(b,v)       SET_BIT(b,v,0x20)  // BYTE3
+#define SET_PROGRAM(b,v)     SET_BIT(b,v,0x40)  // BYTE3
+#define SET_INSTANT(b,v)     SET_BIT(b,v,0x80)  // BYTE3
 
 #define GET_BIT(b,m)         ((b) & (m))
+#define GET_FIRE(b)          GET_BIT(b,0x01)    // BYTE2
+#define GET_CHECK_FLAG(b)    GET_BIT(b,0x02)    // BYTE2
+#define GET_SYSTEM_FLAG(b)   GET_BIT(b,0x04)    // BYTE2
+#define GET_AC_LOSS(b)       GET_BIT(b,0x08)    // BYTE2
 #define GET_READY(b)         GET_BIT(b,0x10)    // BYTE2
+#define GET_ALARM_ZONE(b)    GET_BIT(b,0x20)    // BYTE2
+#define GET_LOWBAT(b,v)      GET_BIT(b,0x40)    // BYTE2  
 #define GET_ARMED_STAY(b)    GET_BIT(b,0x80)    // BYTE2
+
+#define GET_IN_ALARM(b)      GET_BIT(b,0x01)    // BYTE3
+#define GET_SYS_ALARM(b)     GET_BIT(b,0x02)    // BYTE3
 #define GET_ARMED_AWAY(b)    GET_BIT(b,0x04)    // BYTE3
-#define GET_CHIME(b)         GET_BIT(b,0x20)    // BYTE3
 #define GET_POWER(b)         GET_BIT(b,0x08)    // BYTE3
+#define GET_BYPASS(b)        GET_BIT(b,0x10)    // BYTE3
+#define GET_CHIME(b)         GET_BIT(b,0x20)    // BYTE3
+#define GET_PROGRAM(b)       GET_BIT(b,0x40)    // BYTE3
+#define GET_INSTANT(b)       GET_BIT(b,0x80)    // BYTE3
 
 #define H2B(a)               ((uint8_t)(((a) >= '0' && (a) <= '9') ? (a) - '0' : (a) - 'A' + 10))
 #define GET_BYTE(c1,c2)      ((uint8_t)((H2B(c1) << 4) | H2B(c2)))
